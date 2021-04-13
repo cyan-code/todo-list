2a7f9403bdf2e2f3077725ee5f166e96109e9385
cyan-code <cyangit@outlook.com>
2021-04-13 18:02:39 +0800

    fix: 修复footer不吸底部问题

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,68 @@
 <template>
-  <div id="app">
-    <Header />
-    <transition 
-      appear
-      appear-active-class="animate__animated animate__fadeIn"
-      enter-active-class="animate__animated animate__fadeInRight" 
-      leave-active-class="animate__animated animate__fadeOutLeft aniamte" 
-      mode="out-in"> 
-      <router-view />
-    </transition>
-    <Footer />
+  <div id="app" class="outer">
+    <div class="wraper">
+      <div class="top">
+        <Header />
+        <transition
+          appear
+          appear-active-class="animate__animated animate__fadeIn"
+          enter-active-class="animate__animated animate__fadeInRight"
+          leave-active-class="animate__animated animate__fadeOutLeft aniamte"
+          mode="out-in"
+        >
+          <router-view />
+        </transition>
+      </div>
+      <Footer class="bottom" />
+    </div>
   </div>
 </template>
 
 <script>
-import Footer from './components/Footer.vue'
-import Header from './components/Header.vue'
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
 export default {
   components: { Header, Footer },
-  name: 'App',
+  name: "App",
   data() {
-    return {
-    }
+    return {};
   },
-  computed: {
-
-  },
+  computed: {},
   beforeCreate: function () {
     // 阻止手机上缩放
-    document.addEventListener('gesturestart', function (event) {
-      event.preventDefault()
-  })
+    document.addEventListener("gesturestart", function (event) {
+      event.preventDefault();
+    });
   },
   created() {
     /* addEventListener("beforeunload", function(e) {
       localStorage.removeItem('isLogin')
     }) */
-  }
-}
+  },
+};
 </script>
 
-<style>
+<style scoped>
+html, body {
+  height: 100%;
+}
+.outer {
+position:absolute;
+top:0;
+right:0;
+bottom:0;
+left:0;
+}
+.wraper {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
 
+}
+.top {
+  flex: 1;
+}
+.bottom {
+  flex: 0
+}
 </style>
